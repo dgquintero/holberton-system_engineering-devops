81a1b39da3c516e8f933ad11c50d1a0f47a9efa9
Daniel Quintero <47568647+dgquintero@users.noreply.github.com>
2020-03-24 09:21:02 -0500

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # Using Puppet, kills a process named killmenow
 
 exec { 'killnow':
+  path     => ['/usr/bin', '/bin', '/sbin', '/usr/local/bin'],
   command  => 'pkill killmenow',
   provider => 'shell',
 }
