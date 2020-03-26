1b7d47a64e5ca3626d81d7c794af11cb114b70d6
dgquintero <dgquintero02@hotmail.com>
2020-03-26 00:57:46 +0000

    update project

--- 2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Using Puppet, kills a process named killmenow
-
-exec { 'killnow':
-  path     => ['/usr/bin', '/bin', '/sbin', '/usr/local/bin'],
-  command  => 'pkill killmenow',
-  provider => 'shell',
-}
