e07777a1a78f64f8f1032f74ec3830b3f6b72c94
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-04 17:04:10 -0500

    adding script that fetch an url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,5 +6,18 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get('https://jsonplaceholder.typicode.com/posts/1', params={})
-    print(r.headers.get("X-Request-Id"))
+    url = 'https://jsonplaceholder.typicode.com'
+    user_id = argv[1]
+    employee = requests.get('{}/users/{}'.format(url, user_id)).json()
+
+    todos = requests.get('{}/users/{}/todos'.format(url, user_id)).json()
+
+    task_list = []
+    for task in todos:
+        if task.get('completed') is True:
+            task_list.append(task.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.format(employee.get('name'),
+                                                          len(task_list),
+                                                          len(todos)))
+    print('\n'.join('\t {}'.format(task) for task in task_list))
