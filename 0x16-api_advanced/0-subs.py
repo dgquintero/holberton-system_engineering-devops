71b192321e6791e832466aef9bf898820f9d099a
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-07 11:48:57 -0500

    pep8 fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,13 @@
 '''
 import requests
 
+
 def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-                 Chrome/70.0.3538.77 Safari/537.36"}
-    req = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                        headers=headers).json()
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+    url = 'https://www.reddit.com/r/{}/about.json'
+    req = requests.get(url.format(subreddit), headers=headers).json()
     countsubr = req.get("data", {}).get("subscribers", 0)
     return countsubr
